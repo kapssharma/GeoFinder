e793ada8b3ac10cfe69cf1d48f533def574db57a
rahulbenner <rahul.benner@digisoftsolution.net>
2022-08-01 14:35:22 +0530

    address-lookup-rahul

--- GeoFinder/GeoFinder.API/Controllers/NominatimController.cs
+++ GeoFinder/GeoFinder.API/Controllers/NominatimController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 
 namespace GeoFinder.API.Controllers
 {
@@ -7,5 +8,33 @@ namespace GeoFinder.API.Controllers
     [ApiController]
     public class NominatimController : ControllerBase
     {
+        private IConfiguration configuration;
+        public NominatimController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        [HttpGet("{osm_id}")]
+        public async Task<IActionResult> lookup(string osm_id)
+        {
+            var obejctResponse = "";
+            string getNominatimBaseURL = this.configuration.GetSection("AppSettings")["GetNominatimBaseURL"];
+            string setNominatimParms = string.Format("lookup?osm_ids={0}", osm_id);
+            getNominatimBaseURL += setNominatimParms;
+            var restClient = new RestClient(getNominatimBaseURL);
+            var request = new RestRequest(getNominatimBaseURL, Method.Get);
+            var response = await restClient.ExecuteAsync(request);
+            
+            if (response.IsSuccessful)
+            {
+
+                obejctResponse = response.Content;
+            }
+            else
+            {
+                throw new HttpRequestException(response.ErrorMessage);
+            }
+            return Ok(obejctResponse);
+        }
     }
 }

3fa4f8e265a5a4e5daa7a9c2efcaa3820c0f09a3
SandeepSingh <sandeep.singh@digisoftsolution.net>
2022-08-03 19:13:23 +0530

    Reverse_API---Sandeep

--- GeoFinder/GeoFinder.API/BadParameterException.cs
+++ GeoFinder/GeoFinder.API/BadParameterException.cs
@@ -2,6 +2,7 @@
 {
     public class BadParameterException : Exception
     {
+        // For exceptiom message
          public BadParameterException(string? message) : base(message) { }
          public BadParameterException(string? message, Exception innerException) : base(message, innerException) { }
          public BadParameterException() : base() { }
--- GeoFinder/GeoFinder.API/Controllers/NominatimController.cs
+++ GeoFinder/GeoFinder.API/Controllers/NominatimController.cs
@@ -10,31 +10,74 @@ namespace GeoFinder.API.Controllers
     public class NominatimController : ControllerBase
     {
         private IConfiguration configuration;
-        public NominatimController(IConfiguration _configuration)
+           public NominatimController(IConfiguration _configuration)
+           {
+               configuration = _configuration;
+           }
+
+
+        [HttpGet]
+        [Route("search")]
+        public async Task<IActionResult> search(string search, string format)
         {
-            configuration = _configuration;
+            var contentResponse = "";
+            string apiEndPoint = this.configuration.GetSection("AppSettings")["NominatimAPIEndPoint"];
+            var SearchUrl = string.Format(apiEndPoint + "search?q={0}&format={1}", search, format);
+            var restClient = new RestClient(SearchUrl);
+            var request = new RestRequest(SearchUrl, Method.Get);
+            var response = await restClient.ExecuteAsync(request);
+            if (response.IsSuccessful)
+            {
+                contentResponse = response.Content;
+            }
+            else
+            {
+                throw new HttpRequestException(response.ErrorMessage);
+            }
+            return Ok(contentResponse);
         }
 
         [HttpGet]
-         public async Task<IActionResult> Reverse(string formate,string latitude,string longitute)
-         {
-               var contentResponse = "";
-              string apiEndPoint = this.configuration.GetSection("AppSettings")["NominatimAPIEndPoint"];
-              string reverseURL = string.Format(apiEndPoint + "reverse?format={0}&lat={1}&lon={2}", formate, latitude, longitute);
-              var restClient = new RestClient(reverseURL);
-              var request = new RestRequest(reverseURL, Method.Get);
-              var response = await restClient.ExecuteAsync(request);
+        public async Task<IActionResult> status()
+        {
+            var contentResponse = "";
+            string apiEndPoint = this.configuration.GetSection("AppSettings")["NominatimAPIEndPoint"];
+            var statusUrl = string.Format(apiEndPoint + "status.php?format=json");
+            var restClient = new RestClient(statusUrl);
+            var request = new RestRequest(statusUrl, Method.Get);
+            var response = await restClient.ExecuteAsync(request);
+            if (response.IsSuccessful)
+            {
+                contentResponse = response.Content;
+            }
+            else
+            {
+                throw new HttpRequestException(response.ErrorMessage);
+            }
+            return Ok(contentResponse);
+        }
 
-              if (response.IsSuccessful)
-              {
-                  contentResponse = response.Content;
-                  new BadParameterException("Response content Ok");
-              }
-              else
-              {
-                  throw new BadParameterException("Response content" + response);
-              }
-               return Ok(contentResponse);
-         }
+        [HttpGet]
+        [Route("Reverse")]
+        public async Task<IActionResult> Reverse(string formate, string latitude, string longitute)
+        {
+            var contentResponse = "";
+            string apiEndPoint = this.configuration.GetSection("AppSettings")["NominatimAPIEndPoint"];
+            string reverseURL = string.Format(apiEndPoint + "reverse?format={0}&lat={1}&lon={2}", formate, latitude, longitute);
+            var restClient = new RestClient(reverseURL);
+            var request = new RestRequest(reverseURL, Method.Get);
+            var response = await restClient.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                contentResponse = response.Content;
+                new BadParameterException("Response content Ok");
+            }
+            else
+            {
+                throw new BadParameterException("Response content" + response);
+            }
+            return Ok(contentResponse);
+        }
     }
 }

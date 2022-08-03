80b86ff6ae5b1dc05ee146e89d6cd64332432fa8
SandeepSingh <sandeep.singh@digisoftsolution.net>
2022-08-03 16:41:47 +0530

    Reverse_API---Sandeep

---
+++ GeoFinder/GeoFinder.API/BadParameterException.cs
@@ -0,0 +1,9 @@
+﻿namespace GeoFinder.API
+{
+    public class BadParameterException : Exception
+    {
+        public BadParameterException(string? message) : base(message) { }
+        public BadParameterException(string? message, Exception innerException) : base(message, innerException) { }
+        public BadParameterException() : base() { }
+    }
+}
--- GeoFinder/GeoFinder.API/Controllers/NominatimController.cs
+++ GeoFinder/GeoFinder.API/Controllers/NominatimController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System.Net.Http.Headers;
 
 namespace GeoFinder.API.Controllers
 {
@@ -7,5 +9,32 @@ namespace GeoFinder.API.Controllers
     [ApiController]
     public class NominatimController : ControllerBase
     {
+        private IConfiguration configuration;
+        public NominatimController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        [HttpGet]
+         public async Task<IActionResult> Reverse(string formate,string latitude,string longitute)
+         {
+               var contentResponse = "";
+              string apiEndPoint = this.configuration.GetSection("AppSettings")["NominatimAPIEndPoint"];
+              string reverseURL = string.Format(apiEndPoint + "reverse?format={0}&lat={1}&lon={2}", formate, latitude, longitute);
+              var restClient = new RestClient(reverseURL);
+              var request = new RestRequest(reverseURL, Method.Get);
+              var response = await restClient.ExecuteAsync(request);
+
+              if (response.IsSuccessful)
+              {
+                  contentResponse = response.Content;
+                  new BadParameterException("Response content Ok");
+              }
+              else
+              {
+                  throw new BadParameterException("Response content" + response);
+              }
+                return Ok(contentResponse);
+         }
     }
 }

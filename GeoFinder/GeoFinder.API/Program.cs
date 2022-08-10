b53904376e68dfb9b36e4bae41d2ce98926c68cd
Ramzan <Ramzan.khan@digisoftsolution.net>
2022-08-10 12:18:16 +0530

    program-file

--- GeoFinder/GeoFinder.API/Program.cs
+++ GeoFinder/GeoFinder.API/Program.cs
@@ -1,6 +1,21 @@
+using GeoFinder.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyWorldDbConnection"));
+});
+
+// For Entity Framework
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnStr")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
